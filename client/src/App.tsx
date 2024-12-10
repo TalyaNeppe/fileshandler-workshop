3ab3371c739089b41f7c580a9cc3dc3da166a706
TalyaNeppe <talyaneppe@hilma.tech>
2024-12-10 16:29:31 +0200

    add instructions and style to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="container">
+      <div className="instructions">
+        <h2>Let's start building our uploading files project :)</h2>
+        <h3>Client side</h3>
+        <ol>
+          <li> Install files package</li>
+          <li> Use basic FileInput component and filesUploader</li>
+          <li>
+            Present the image before sending it to the server Advance options:
+            add handle for errors, delete file, handle multiple files with
+            MultipleFileInput
+          </li>
+        </ol>
+        <a
+          href="https://hilma.atlassian.net/wiki/spaces/TD/pages/83362528/Hilma+Fileshandler#client"
+          target="blank"
+        >
+          Link to docs
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <hr />
+        <h3>Server side</h3>
+        <ol>
+          <li> Install files package</li>
+          <li>Configure the fileshandler module</li>
+          <li>
+            Generate server functions within the controller and service to save
+            the image
+          </li>
+          <h4>Connect between client & server</h4>
+          <li>Create a request to the endpoint created in the previous step</li>
+          <li>
+            Display the saved image on the client-side once it's stored on the
+            server
+          </li>
+          <li>
+            Advance options: save multiple files, save files with different
+            types (image and file)
+          </li>
+        </ol>
+        <a
+          href="https://hilma.atlassian.net/wiki/spaces/TD/pages/83362528/Hilma+Fileshandler#server"
+          target="blank"
+        >
+          Link to docs
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="work-page">
+        <h2>Upload files here</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
